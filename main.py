fd9e1b05d1c027c4a0d32c8036cf90a27c711b37
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-12-10 09:25:42 -0600

    initial skeleton

---
+++ main.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://adriangb.com/test"
+
+
+def are_headers_equal(*file_urls: str) -> bool:
+    raise NotImplementedError
+
+
+def are_files_equal(*file_urls: str) -> bool:
+    raise NotImplementedError
