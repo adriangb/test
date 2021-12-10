36f5a0289ed98c56eb286e53f8f5ef4482c47295
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-12-10 10:03:53 -0600

    make files smaller

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-BASE_URL = "https://adriangb.com/test"
+import requests
 
 
-def are_headers_equal(*file_urls: str) -> bool:
+def are_headers_equal(file_url: str, *file_urls: str) -> bool:
+    raise NotImplementedError
+
+def are_files_equal(file_url: str, *file_urls: str) -> bool:
     raise NotImplementedError
 
 
-def are_files_equal(*file_urls: str) -> bool:
-    raise NotImplementedError
+if __name__ == "__main__":
+    files = [f.strip() for f in open("files.txt").readlines()]
+    print(are_headers_equal(*files))
+    print(are_files_equal(*files))
